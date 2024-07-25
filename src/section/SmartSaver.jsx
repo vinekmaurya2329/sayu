f12e1899233d1f852a60595ea8107d817b4a48c4
vinekmaurya2329 <vinekmaurya2329@gmail.com>
2024-07-25 16:58:09 +0530

    home page

--- src/section/SmartSaver.jsx
+++ src/section/SmartSaver.jsx
@@ -7,8 +7,8 @@ function SmartSaver() {
         <div className="smart-saver-upper-container">
             <div className="blank"></div>
    <button>Smart saver</button>
-   <h3>SAYV™ is a Personal Financial Management (“PFM”) </h3>
-   <h3>application for desktop, iOS and Android systems</h3>
+   <h3>SAYV™ is a Personal Financial Management (“PFM”) <br /> application for desktop, iOS and Android systems </h3>
+   {/* <h3>application for desktop, iOS and Android systems</h3> */}
    <p>Available on desktop, laptop, iOS, and Android. SAYV offers tailored money-saving and cost-of-living benefits across various daily life categories. It captures your personal info, assesses your financial and lifestyle position, and makes budget and lifestyle recommendations. Monitor and adjust your outcomes easily.</p>
         </div>
         <div className="smart-saver-middle-container">
@@ -20,11 +20,11 @@ function SmartSaver() {
           <p>Create personalised dashboards and reports to focus on the financial details that matter most to you.</p>
             </div>
             <div className="middle-right">
-                  <img src="../..//SayVhomeMobileimage.png" alt="" />
+                  <img src="../..//Sayv-image-1.png" alt="" />
             </div>
         </div>
         <div className="smart-saver-lower-container">
-                 <img src="https://s3-alpha-sig.figma.com/img/ecfc/cb87/d9318b380f1f715d4d4e5de3c05e6db5?Expires=1722816000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=cC1Ivy8jvF5HGAqV3znB3LRxWV1ZZRUAU-drqWX-p3shAuKvMwZZi-EtIkNYYg7CTugZK7y-Zv1~4b1hMcjHckjT0gVaGLGoeyhZeM5QEQI6Btc4r5QCJ6iZsWqgSJrVwtCxwAzYE4DwVzHDvstl6wrtLlJH6mV5Ziz91M84RSxNuVzLtwjK~ZJNriAaYX1hXnT4nY0-~i1Wwmj68UVCIUOsUa1pa8tnbP17glwYNS0fu7-J3jnnR4-gOTsQsmt0LaP48XZptzZmQud0lnDUFMKsibQv4B8MKCiLdWrEZIHfND0TD9Trlam9fXEZYcY6b-oqc0q7w9Uv0oZFIMtmyw__" alt="" />
+                 <img src="../..//Sayv-image-2.png" alt="" />
         </div>
        
   <div className="smart-saver-bottom">
